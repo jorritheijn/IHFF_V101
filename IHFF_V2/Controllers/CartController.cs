3ebee4654f868f882b960f011c079b44fa2100be
Jamal Doorson <JamalDoorson@live.nl>
2017-01-05 09:53:10 +0100

    Winkelwagen shit [WIP]

---
+++ IHFF_V2/Controllers/CartController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IHFF_V2.Models;
+
+namespace IHFF_V2.Controllers
+{
+    public class CartController : Controller
+    {
+        //Shows all items currently in the cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Function for adding items to cart
+        //HAS TO BE USED IN //OTHER// CONTROLLERS!
+        public ActionResult Order()
+        {
+            if(Session["cart"] == null)
+            {
+                List<CartItem> cart = new List<CartItem>();
+                cart.Add(new CartItem());
+                Session["cart"] = cart;
+            }
+            else
+            {
+
+            }
+            return View();
+        }
+    }
+}
