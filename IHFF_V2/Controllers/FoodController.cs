94cc49c954d8242639244158ca6f4f696670bd08
Tim Ranzijn <566810@student.inholland.nl>
2017-01-06 01:16:37 +0100

    +Specialoverzicht

---
+++ IHFF_V2/Controllers/FoodController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IHFF_V2.Controllers
+{
+    public class FoodController : Controller
+    {
+        //
+        // GET: /Food/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
