4ff025c96a39637b3b9bfaa825cfd78ec01712b3
Jamal Doorson <JamalDoorson@live.nl>
2017-03-31 09:12:47 +0200

    Composite Primary Key toegevoegt aan BestelRegels. Commentaar CartController.

--- IHFF_V2/Controllers/CartController.cs
+++ IHFF_V2/Controllers/CartController.cs
@@ -10,7 +10,10 @@ namespace IHFF_V2.Controllers
 {
     public class CartController : Controller
     {
-        //Shows all items currently in the cart
+        /// <summary>
+        /// Een overzicht van alle items aanwezig in de winkelwagen
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             return View();
@@ -26,14 +29,17 @@ namespace IHFF_V2.Controllers
         //    Session["cart"] = cart;
         //    return View("Index");
         //}
-
-        //Increments the quantity of a cart item
+        
+        /// <summary>
+        /// Het aantal reserveringen voor een bepaald item wordt verhoogt met 1
+        /// </summary>
+        /// <param name="id">Het id van een CartItem</param>
+        /// <returns>Terug naar het overzicht van de winkelwagen wanneer succesvol. Naar de error-page wanneer niet succesvol</returns>
         public ActionResult Increment(int id)
         {
             try
             {
-                List<CartItem> cart = GetCartFromSession();
-                CartItem ci = cart.First(c => c.Id == id);
+                CartItem ci = GetCartItem(id);
                 ci.Quantity++;
             }
             catch(Exception e)
@@ -42,14 +48,20 @@ namespace IHFF_V2.Controllers
             }
             return View("Index");
         }
-
-        //Decrements the quantity of a cart item. Deletes if there's too few items in the cart
+        
+        /// <summary>
+        /// Het aantal reserveringen voor een bepaald item wordt verlaagt met 1
+        /// Als het aantal op 0 komt, wordt het item automatisch verwijdert uit de winkelwagen
+        /// </summary>
+        /// <param name="id">Het id van een CartItem</param>
+        /// <returns>Terug naar het overzicht van de winkelwagen wanneer succesvol. Naar de error-page wanneer niet succesvol</returns>
         public ActionResult Decrement(int id)
         {
             try
             {
-                List<CartItem> cart = GetCartFromSession();
-                CartItem ci = cart.First(c => c.Id == id);
+                //List<CartItem> cart = GetCartFromSession();
+                //CartItem ci = cart.First(c => c.Id == id);
+                CartItem ci = GetCartItem(id);
                 if (ci.Quantity > 1)
                     ci.Quantity--;
                 else
@@ -61,8 +73,12 @@ namespace IHFF_V2.Controllers
             }
             return View("Index");
         }
-
-        //Removes an item from the cart
+        
+        /// <summary>
+        /// Verwijdert een item uit de winkelwagen
+        /// </summary>
+        /// <param name="id">Het id van een CartItem</param>
+        /// <returns>Terug naar het overzicht van de winkelwagen wanneer succesvol. Naar de error-page wanneer niet succesvol</returns>
         public ActionResult Delete(int id)
         {
             try
@@ -78,25 +94,40 @@ namespace IHFF_V2.Controllers
             //Session["cart"] = cart; //Is niet nodig?
             return View("Index");
         }
-
-        //Calls Index after adding another item to the cart
+        
+        /// <summary>
+        /// Probeert een nieuw item toe te voegen aan de winkelwagen. Als er als een instantie van het item aanwezig is, wordt de qty
+        /// opgeteld bij het bestaande item
+        /// </summary>
+        /// <param name="id">Het id van een CartItem</param>
+        /// <param name="quantity">Het aantal reserveringen dat wordt geplaatst (of toegevoegt)</param>
+        /// <returns>Terug naar het overzicht van de winkelwagen wanneer succesvol. Naar de error-page wanneer niet succesvol</returns>
         public ActionResult Order(int id, int quantity)
         {
-            if (Session["cart"] == null)
+            try
             {
-                List<CartItem> cart = new List<CartItem>();
-                cart.Add(new CartItem(id, quantity));
-                Session["cart"] = cart;
-            }
-            else
-            {
-                List<CartItem> cart = (List<CartItem>)Session["cart"];
-                CartItem ci = cart.FirstOrDefault(c => c.Id == id);
-                if (ci == null)
+                //Controlleer of er al een winkelwagen is
+                if (Session["cart"] == null)
+                {
+                    List<CartItem> cart = new List<CartItem>();
                     cart.Add(new CartItem(id, quantity));
+                    Session["cart"] = cart;
+                }
                 else
-                    ci.Quantity += quantity;
-                Session["cart"] = cart;
+                {
+                    List<CartItem> cart = (List<CartItem>) Session["cart"];
+                    CartItem ci = cart.FirstOrDefault(c => c.Id == id);
+                    //Controlleer of het gegeven item al aanwezig is
+                    if (ci == null)
+                        cart.Add(new CartItem(id, quantity));
+                    else
+                        ci.Quantity += quantity;
+                    Session["cart"] = cart;
+                }
+            }
+            catch (Exception e)
+            {
+                return View("Error");
             }
             return View("Index");
         }
@@ -134,14 +165,29 @@ namespace IHFF_V2.Controllers
             return View();
         }
 
+        public CartItem GetCartItem(int id)
+        {
+            List<CartItem> cart = GetCartFromSession();
+            return cart.First(c => c.Id == id);
+        }
+
+        /// <summary>
+        /// Zet de sessionvariabele om in een bruikbare lijst van CartItems
+        /// </summary>
+        /// <returns>Een lijst van CartItems</returns>
         private List<CartItem> GetCartFromSession()
         {
             return (List<CartItem>)Session["cart"];
         }
-
-        //Calculate and return the price of the cart
+        
+        /// <summary>
+        /// Berekent de totaalprijs van de huidige winkelwagen
+        /// </summary>
+        /// <returns>De totaalprijs van de winkelwagen</returns>
         private float CalculateCartValue()
         {
+            //TODO: Houdt rekening met restaurantreserveringen!
+
             List<CartItem> cart = (List<CartItem>)Session["cart"];
 
             float totaal = 0;
