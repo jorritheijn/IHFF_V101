3a19e5cc35d6b3c5a2cba3787e54dd855e3de1c9
Tim Ranzijn <566810@student.inholland.nl>
2017-04-02 00:30:40 +0200

    dingen teruggezet enzo

--- IHFF_V2/Controllers/SpecialController.cs
+++ IHFF_V2/Controllers/SpecialController.cs
@@ -15,53 +15,76 @@ namespace IHFF_V2.Controllers
         // GET: /SpecialOverzicht/
 
         private IspecialRepository SpecialRepository = new SpecialRepository();
+        private EventRepository events = new EventRepository();
+        private IEnumerable<Event> specials = new EventRepository().GetEventsOfType("Special");
 
         public ActionResult Index(string searchString, string dag)
         {
-
-            //Default
-            //Geselecteerde events zijn de events die we in de view willen weergeven
-            IEnumerable<Event> geselecteerdeEvents = new SpecialRepository().AlleSpecialsEnkel;
-
-            //Als er op een dagfilter geklikt is.
-            if (dag != null)
-            {
-                //verwijst door naar ActionResult DagProgramma hieronder. 
-                return RedirectToAction("DagProgramma", new { dag = dag });
-            }
-
             // als er gebruik is gemaakt van het searchfilter
             if (!String.IsNullOrEmpty(searchString))
             {
                 //pas geselecteerdeEvents aan naar naar enkel de resultaten die het zoekwoord bevatten
-                geselecteerdeEvents = new SpecialRepository().SpecailsOpZoekWoord(searchString, geselecteerdeEvents);
+                specials = specials.Where(s => s.Titel.Contains(searchString));
             }
-
-            return View(geselecteerdeEvents);
+            return View(specials);
         }
+
 
         //Als er op een DagProgramma
 
         public ActionResult DagProgramma(string dag, string searchString)
         {
-            IEnumerable<Event> GefilterdeEvents = new SpecialRepository().SpecialsOpDag(dag);
+            specials = SpecialsOpDag(dag, specials);
 
             if (!String.IsNullOrEmpty(searchString))
             {
-                GefilterdeEvents = new SpecialRepository().SpecailsOpZoekWoord(searchString, GefilterdeEvents);
+                specials = specials.Where(s => s.Titel.Contains(searchString));
             }
             ViewBag.Dag = dag;
-            return View(GefilterdeEvents);
+            return View(specials);
         }
+
+        public IEnumerable<Event> SpecialsOpDag(string dag, IEnumerable<Event> specials)
+        {
+            DayOfWeek AangeklikteDag = DayOfWeek.Monday;
+            switch (dag)
+            {
+                case "maandag":
+                    AangeklikteDag = DayOfWeek.Monday;
+                    break;
+
+                case "dinsdag":
+                    AangeklikteDag = DayOfWeek.Tuesday;
+                    break;
+
+                case "woensdag":
+                    AangeklikteDag = DayOfWeek.Wednesday;
+                    break;
+
+                case "donderdag":
+                    AangeklikteDag = DayOfWeek.Thursday;
+                    break;
+
+                case "vrijdag":
+                    AangeklikteDag = DayOfWeek.Friday;
+                    break;
+
+                case "zaterdag":
+                    AangeklikteDag = DayOfWeek.Saturday;
+                    break;
+
+                case "zondag":
+                    AangeklikteDag = DayOfWeek.Sunday;
+                    break;
+            }
+            return specials.Where(s => s.Tijd.Value.DayOfWeek.Equals(AangeklikteDag));
+        }
+
 
         //brian's code below
         //
         //
 
-        public ActionResult Index(int Id)
-        {
-            return View();
-        }
 
         public ActionResult DetailSpecialpage(int Id)
         {
