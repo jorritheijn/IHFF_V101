bba3737f881652ba994123b8395e7f05e84a2117
Jorrit Heijn <jorrit_heijn@hotmail.com>
2017-04-05 12:32:39 +0200

    commitje

--- IHFF_V2/Repositories/EventRepository.cs
+++ IHFF_V2/Repositories/EventRepository.cs
@@ -10,24 +10,20 @@ namespace IHFF_V2.Repositories
     public class EventRepository
     {
         private ihffContext ctx;
-
         public EventRepository()
         {
             ctx = new ihffContext();
         }
-
         public Event GetEvent(int id)
         {
             return ctx.Events.First(e => e.Id == id);
         }
-
         public Event GetCorrectEventForCart(int id, DateTime? tijd) {
 
             string titel = ctx.Events.Where(a => a.Id == id).Select(a => a.Titel).SingleOrDefault();
 
             return ctx.Events.Where(a => a.Titel == titel && a.Tijd == tijd).SingleOrDefault();
         }
-
         public IEnumerable<Event> GetAllEvents()
         {
             IEnumerable<Event> films = ctx.Events.Where(film => film.Type.Equals("Film")).GroupBy(x => x.Titel).Select(x => x.FirstOrDefault());
@@ -37,30 +33,25 @@ namespace IHFF_V2.Repositories
             IEnumerable<Event> events = films.Concat(restaurants).Concat(cultuurevents).Concat(specials);
             return events;
         }
-
         //Haalt events op van het type dat is meegeven in de parameter. Haalt niet meer dan een event op met dezelde titel.
         public IEnumerable<Event> GetEventsOfType(string type)
         {
                     return ctx.Events.Where(film => film.Type.Equals(type)).GroupBy(x => x.Titel).Select(x => x.FirstOrDefault());
         }
-
         //Haalt alle events op van het type dat is meegegeven in de parameter.
         public IEnumerable<Event> GetAllEventsOfType(string type)
         {
             return ctx.Events.Where(film => film.Type.Equals(type));
         }
-
         public IEnumerable<Event> KrijgEvents() 
         {
             return ctx.Events;
         }
-
         public void AddEvent(Event eventItem)
         {
             ctx.Events.Add(eventItem);
             ctx.SaveChanges();
         }
-
         public void DeleteEvent(int id)
         {
             Event eventItem = new Event();
